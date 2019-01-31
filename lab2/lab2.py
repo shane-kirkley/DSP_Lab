fc886eeea24d626ee6b6cb132c30c978fe2ebaa8
Shane <shane.kirkley@colorado.edu>
2019-01-30 20:16:22 -0700

    grayscale conversion added

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,19 +1,20 @@
 import numpy as np
 from PIL import Image
 
+Y = np.array([.299, .587, .114])
+
+# converts array of rgb image data to grayscale
+def grayscale(img):
+    return np.clip(np.dot(img[:,:,:3], Y), 0, 255)
+
 pil_image_in = Image.open('test01.jpg')
 image_in = np.asarray(pil_image_in, np.uint8)
-print(f"shape of image = {image_in.shape}")
 
-rows = image_in.shape[0]
-cols = image_in.shape[1]
-
-print(f"rows = {rows}; cols = {cols}")
-
-# modify image here as appropriate
+image_out = grayscale(image_in)
 
 # Output the image to a file
-Image.fromarray(image_in).save('test_img_out.jpg')
+# getting error 'cannot write mode F as JPEG', convert('L') fixes it
+Image.fromarray(image_out).convert('L').save('test_img_out1.jpg')
 
 # view the image
-Image.fromarray(image_in).show()
+Image.fromarray(image_out).show()
