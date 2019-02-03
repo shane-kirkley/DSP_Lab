096f27f82965e97d007d7bdccf3205da0ae4a498
Shane <shane.kirkley@colorado.edu>
2019-02-03 12:40:59 -0700

    edge detection, up/down sampling stubs added

--- lab2/contrast.py
+++ lab2/contrast.py
@@ -18,3 +18,34 @@ plt.xlabel('Intensity')
 plt.ylabel('Number of Pixels')
 plt.title('Histogram')
 plt.show()
+
+S = histogram[1:255].sum()
+P = S / 254
+
+remap = np.zeros(256)
+remap[0] = histogram[0]
+remap[255] = histogram[255]
+
+T = P
+curr_sum = 0
+out_val = 1
+last_idx = 1
+
+
+# this loop creates remap array and remaps pixels of image
+for intensity in range(1,255):
+    curr_sum = curr_sum + histogram[intensity]
+    remap[intensity] = out_val
+    imdat[np.where(imdat == intensity)] = out_val
+    if curr_sum > T:
+        out_val = round(curr_sum / P)
+        T = out_val * P
+
+plt.plot(remap)
+plt.xlabel('Intensity')
+plt.ylabel('Number of Pixels')
+plt.title('Remapped Histogram')
+plt.show()
+
+Image.fromarray(imdat).convert('L').save('test_img_contrast1.jpg')
+Image.fromarray(imdat).show()
---
+++ lab2/edge_detection.py
@@ -0,0 +1,29 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import sys
+from scipy import signal
+
+kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+pilim = Image.open(sys.argv[1])
+
+if sys.argv[2]:
+    out_filename = sys.argv[2]
+else:
+    out_filename = 'test_img_edge.jpg'
+
+pilim = pilim.convert('L')  # convert to monochrome
+imdat = np.asarray(pilim, np.float) # put PIL image into numpy array of floats
+
+# it isn't necessary to have a pair of nested loops that process each pixel seperately
+grad_x = signal.convolve2d(imdat, kernel_x)
+grad_y = signal.convolve2d(imdat, kernel_y)
+
+#TODO: how to implement threshold?
+
+edge = np.sqrt(grad_x**2 + grad_y**2)
+
+Image.fromarray(edge).convert('L').save(out_filename)
+Image.fromarray(edge).show()
