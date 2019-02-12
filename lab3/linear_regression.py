b25c2856af8ec2d1eb48d8471cbd96c5cab9a0e7
shane-kirkley <shane.kirkley@colorado.edu>
2019-02-12 13:30:13 -0700

    lab3 started

---
+++ lab3/linear_regression.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sys
+import math
+
+# perform linear regression to predict the sujectively reported
+# hunger based on the electrode data. Write code to calculate the
+# psuedo-inverse yourself (don't use library call).
+
