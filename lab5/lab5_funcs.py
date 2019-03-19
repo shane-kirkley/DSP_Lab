f5179a6331818a321fcf700ce03213df2d3fe803
shane-kirkley <shane.kirkley@colorado.edu>
2019-03-19 16:17:42 -0600

    lab 5 init

---
+++ lab5/lab5_funcs.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+import numpy as np
+import math
+import sys
+from PIL import Image
+
+def dctmgr():
+    raise NotImplementedError
+
+def idctmgr():
+    raise NotImplementedError
