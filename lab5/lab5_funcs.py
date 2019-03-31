897a3e1446ab99142f58ba7db346f7c246a8708c
Shane <shane.kirkley@colorado.edu>
2019-03-31 14:46:29 -0600

    forward 2d dct added

--- lab5/lab5_funcs.py
+++ lab5/lab5_funcs.py
@@ -3,9 +3,29 @@ import numpy as np
 import math
 import sys
 from PIL import Image
+from scipy.fftpack import dct
 
-def dctmgr():
+"""
+Forward DCT for use on 2D arrays. Applies DCT to rows then columns.
+"""
+def forward_dct(input):
+    return dct(dct(input).T).T
+
+"""
+Lab assignment 1
+Input: 2D array of grayscale image data
+Output: Coefficient array of size 64 x N, where N is number of 8x8 blocks in image.
+"""
+def dctmgr(img_data):
+    # divide into non-overlapping 8x8 blocks
+    # transform each block
+    # Write forward 2d DCT routine
     raise NotImplementedError
 
-def idctmgr():
+"""
+Lab assignment 2
+Input: Coefficient array of size 64 x N
+Output: 2D array of grayscale image data.
+"""
+def idctmgr(coeff):
     raise NotImplementedError
